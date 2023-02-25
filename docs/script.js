277926658de06148cae0af70b1447979c3531346
Nate Hammer <nhammer514@gmail.com>
2023-02-24 19:48:23 -0500

    website updated with resume, picture, and extra content

---
+++ docs/script.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded',init,false);
+function init(){
+    var buttons = document.getElementsByTagName("a");
+    for (var i = 0; i <  buttons.length; i++){
+        buttons[i].addEventListener('click', setMenu, false);
+    }
+}
+
+function setMenu(){
+    hidePages();
+    var sectionID =  "page-" + this.id;
+    var selSection = document.getElementById(sectionID)
+    selSection.style.display = 'block';
+    topHeader.textContent = this.id;
+}
+
+function hidePages(){
+    var sections = document.getElementsByClassName('page')
+    for (var p = 0; p < sections.length; p++){
+        sections[p].style.display='none';
+    }
+}
