f544a7e4b99516522011729f95540ab70fcfafb5
Nate Hammer <nhammer514@gmail.com>
2023-04-05 15:47:19 -0400

    corpus analysis

--- docs/textfile_script.js
+++ docs/textfile_script.js
@@ -5,14 +5,6 @@ function init(){
         buttons[i].addEventListener('click', toggle, false);
     }
 }
-
-function setMenu(){
-    hidePages();
-    var sectionID =  "page-" + this.id;
-    var selSection = document.getElementById(sectionID)
-    selSection.style.display = 'block';
-}
-
 function toggle(){
     var sectionID =  "section-" + this.id;
     var selSection = document.getElementById(sectionID);
@@ -21,12 +13,5 @@ function toggle(){
     }
     else{
         selSection.style.display = 'block';
-    }
-}
-
-function hidePages(){
-    var sections = document.getElementsByClassName('page')
-    for (var p = 0; p < sections.length; p++){
-        sections[p].style.display='none';
     }
 }
